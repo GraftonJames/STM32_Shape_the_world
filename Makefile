f0738f35f1302d86caccc1d491fa190c2d7a78a9
James Grafton <james.grafton9@gmail.com>
2025-03-08 13:56:49 +0000

    using CMSIS now and SYSticks

--- Makefile
+++ Makefile
@@ -2,15 +2,23 @@
 
 CFLAGS  ?=  -W -Wall -Wextra -Werror -Wundef -Wshadow -Wdouble-promotion \
             -Wformat-truncation -fno-common -Wconversion \
-            -ggdb -Os -ffunction-sections -fdata-sections -I. \
+            -g3 -Os -ffunction-sections -fdata-sections \
+            -I. -Iinclude -Ivendor/cmsis_arm_v6/CMSIS/Core/Include -Ivendor/cmsis_wb/Include \
             -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 $(EXTRA_CFLAGS)
-LDFLAGS ?= -Tlink.ld -nostartfiles -nostdlib --specs nano.specs -lc -lgcc -Wl,--gc-sections -Wl,-Map=$@.map
-SOURCES = main.c
+LDFLAGS ?= -Tvendor/cmsis_wb/Source/Templates/gcc/linker/stm32wb15xx_flash_cm4.ld -nostartfiles -nostdlib --specs nano.specs -lc -lgcc -Wl,--gc-sections -Wl,-Map=$@.map
+SOURCES = main.c 
+SOURCES += vendor/cmsis_wb/Source/Templates/gcc/startup_stm32wb15xx_cm4.s
 
-build: firmware.elf
+build: firmware.bin
 
-firmware.elf: $(SOURCES)
-> arm-none-eabi-gcc $(SOURCES) $(CFLAGS) $(LDFLAGS) -o $@
+cmsis_arm_v6:
+> git clone --depth 1 -b "v6.1.0" "https://github.com/ARM-software/CMSIS_6.git" vendor/$@
+
+cmsis_wb:
+> git clone --depth 1 -b "v1.12.2" "https://github.com/STMicroelectronics/cmsis-device-wb.git" vendor/$@
+
+firmware.elf: $(SOURCES) 
+> bear -- arm-none-eabi-gcc $(SOURCES) $(CFLAGS) $(LDFLAGS) -o $@
 
 firmware.bin: firmware.elf
 > arm-none-eabi-objcopy -O binary $< $@
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-
+#include "stm32wb15xx.h"
 
 #define BIT(x) (1UL << (x))
 #define PIN(bank, num) ((((bank) - 'A') << 8) | (num))
@@ -9,99 +9,33 @@
 #define PINBANK(pin) (pin >> 8)
 
 enum {GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG};
-
-struct gpio {
-	volatile uint32_t 
-		MODER,
-		OTYPER,
-		OSPEEDR,
-		PUPDR,
-		IDR,
-		ODR,
-		BSRR,
-		LCKR,
-		AFRL, 
-		AFRH,
-		BRR;
-};
-#define GPIO(bank) (((struct gpio *) 0x48000000 + 0x400 * (bank)))
-
-struct rcc { // Reset and Clock Control
-	volatile uint32_t
-		CR,
-		ICSCR,
-		CFGR,
-		PLLCFGR,
-		_RESERVED1[2],
-		CIER,
-		CIFR,
-		CICR,
-		SMPSCR,
-		AMB1RSTR,
-		AHB2RSTR,
-		AHB4RSTR,
-		_RESERVED2[1],
-		APB1RSTR1,
-		APB1RSTR2,
-		APB2RSTR,
-		APB3RSTR,
-		AHB1ENR,
-		AHB2ENR,
-		AHB4ENR,
-		_RESERVED3[1],
-		APB1ENR1,
-		APB1ENR2,
-		APB2ENR,
-		_RESERVED4[1],
-		AHB1SMENR,
-		AHB2SMENR,
-		AHB4SMENR,
-		_RESERVED5[1],
-		APB1SMENR1,
-		APB1SMENR2,
-		APB2SMENR,
-		_RESERVED6[1],
-		CCIPR,
-		_RESERVED7[1],
-		BDCR,
-		CSR,
-		HSECR,
-		_RESERVED8[26],
-		EXTCFGR,
-		_RESERVED9[15],
-		C2AHB1ENR,
-		C2AHB2ENR,
-		C2AHB4ENR,
-		_RESERVED10[1],
-		C2APB1ENR1,
-		C2APB1ENR2,
-		C2APB3ENR,
-		C2AHB1SMENR,
-		C2AHB2SMENR,
-		C2AHB4SMENR,
-		_RESERVED11[1],
-		C2APB1SMENR1,
-		C2APB1SMENR2,
-		C2APB2SMENR,
-		C2APB3SMENR;
-};
-#define RCC ((struct rcc *) 0x58000000)
+#define GPIO(bank) ((GPIO_TypeDef *) (GPIOA_BASE + 0x400U * (bank))) 
 
 static inline void
 gpio_set_mode(uint16_t pin, uint8_t mode)
 {
-	struct gpio *gpio = GPIO(PINBANK(pin));
+	GPIO_TypeDef *gpio = GPIO(PINBANK(pin));
 	uint8_t n = PINNO(pin);
 
 	gpio->MODER &= ~(3U << (n * 2));
-	gpio->MODER |=	(mode & 3) << (pin * 2);
+	gpio->MODER |=	(mode & 3) << (n * 2);
 }
 
 static inline void
 gpio_write(uint16_t pin, bool val)
 {
-	struct gpio *gpio = GPIO(PINBANK(pin));
+	GPIO_TypeDef *gpio = GPIO(PINBANK(pin));
 	gpio->BSRR = (1U << PINNO(pin)) << (val ? 0 : 16);
+}
+
+static inline void
+systick_init(uint32_t ticks)
+{
+	if ((ticks - 1) > 0xffffff) return;
+	
+	SysTick->LOAD = ticks - 1;
+	SysTick->VAL = 0;
+	SysTick->CTRL = BIT(0) | BIT(1) | BIT(2);
 }
 
 static inline void
@@ -109,33 +43,53 @@ spin(volatile uint32_t count)
 {
 	while (count--) (void) 0;
 }
+static volatile uint32_t s_ticks;
+void
+delay(unsigned ms)
+{
+	uint32_t until = s_ticks + ms;
+	while (s_ticks < until) (void) 0;
+}
+
+bool
+timer_expired(uint32_t *t, uint32_t prd, uint32_t now)
+{
+	if (now + prd < *t) *t = 0;
+	if (*t == 0) *t = now + prd;
+	if (*t > now) return false;
+	*t = (now - *t) > prd ? now + prd : *t + prd;
+	return true;
+}
 
 int
 main(void) {
-	uint16_t led = PIN('B', 7);
-	RCC->AHB1ENR |= BIT(PINBANK(led));
+
+	uint16_t led = PIN('B', 5);
+	RCC->AHB2ENR |= BIT(PINBANK(led));
+	systick_init(16000000/1000);
 	gpio_set_mode(led, GPIO_MODE_OUTPUT);
+
+
+	uint32_t timer, period = 500;
 	for (;;) {
-		gpio_write(led, true);
-		spin(999999);
-		gpio_write(led, false);
-		spin(999999);
+		if (timer_expired(&timer, period, s_ticks)) {
+			static bool on;
+			gpio_write(led, on);
+			on = !on;
+		}
 	}
 	return 0;
 }
 
-//Startup code
-__attribute__((naked, noreturn)) void _reset(void) {
-	extern long _sbss, _ebss, _sdata, _edata, _sidata;
-	for (long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
-	for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
-
-	main();
-	for (;;) (void) 0;
+void
+SysTick_Handler(void) {
+	s_ticks++;
 }
 
-extern void _estack(void);
+void SystemInit(void)
+{
+}
 
-__attribute__((section(".vectors"))) void (*const tab[16 + 63])(void) = {
-	_estack, _reset
-};
+void SystemCoreClockUpdate(void)
+{
+}
